eb76a2252b2e164aed6c1cefecc81398a026fd8e
andycai <huayicai@gmail.com>
2023-09-17 10:11:38 +0800

    清理，删除 top-level-await 插件

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import topLevelAwait from "vite-plugin-top-level-await";
+// import topLevelAwait from "vite-plugin-top-level-await";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
   plugins: [
     vue(),
-    topLevelAwait({
-      promiseExportName: '__tla',
-      promiseImportName: i => `__tla_${i }`
-    })
+    // topLevelAwait({
+    //   promiseExportName: '__tla',
+    //   promiseImportName: i => `__tla_${i }`
+    // })
   ],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
